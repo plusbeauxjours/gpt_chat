7b043cbb9eb5afd9bcc09de9b8c296d47cc8854b
plusbeauxjours <plusbeauxjours@gmail.com>
2023-12-13 12:45:10 +0700

    set: practice methods

--- Home.py
+++ Home.py
@@ -1,11 +1,30 @@
 import streamlit as st
+from datetime import datetime
 
-st.title("Hello world!")
+today = datetime.today().strftime("%H:%M:%S")
 
-st.subheader("Welcome to Streamlit!")
+st.title(today)
 
-st.markdown(
-    """
-    #### I love it!
-"""
+
+model = st.selectbox(
+    "Choose your model",
+    (
+        "GPT-3",
+        "GPT-4",
+    ),
 )
+
+if model == "GPT-3":
+    st.write("cheap")
+else:
+    st.write("not cheap")
+    name = st.text_input("What is your name?")
+    st.write(name)
+
+    value = st.slider(
+        "temperature",
+        min_value=0.1,
+        max_value=1.0,
+    )
+
+    st.write(value)
