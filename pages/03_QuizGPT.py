98a73866eb9a021b2a12567d891f6bcd62aaf85d
plusbeauxjours <plusbeauxjours@gmail.com>
2024-01-29 14:40:16 +0700

    set: Grading Questions

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -247,16 +247,16 @@ if not docs:
     )
 else:
     response = run_quiz_chain(docs, topic if topic else file.name)
-    with st.form("question_form"):
-        for question in response["question"]:
+    with st.form("questions_form"):
+        for question in response["questions"]:
             st.write(question["question"])
             value = st.radio(
-                "Select an options.",
+                "Select an option.",
                 [answer["answer"] for answer in question["answers"]],
                 index=None,
             )
-        if {"ansswer": value, "correct": True}:
-            st.suceess("Correct")
-        elif value is not None:
-            st.error("Wrong!")
-    button = st.form_submit_button()
+            if {"answer": value, "correct": True} in question["answers"]:
+                st.success("Correct!")
+            elif value is not None:
+                st.error("Wrong!")
+        button = st.form_submit_button()
