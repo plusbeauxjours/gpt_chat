f33d907ac54ed49a0ee4f8f4c3ac59d0bd56fbcd
plusbeauxjours <plusbeauxjours@gmail.com>
2024-02-07 17:55:37 +0700

    set: FastAPI Server

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from langchain.llms.openai import OpenAI
-from langchain.chat_models import ChatOpenAI
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
-llm = OpenAI()
-a = llm.predict("How many planets are there?")
+app = FastAPI(
+    title="Nicolacus Maximus Quote Giver",
+    description="Get a real quote said by Nicolacus Maximus himself.",
+)
 
-chat = ChatOpenAI()
-b = chat.predict("How many planets are there?")
 
-a, b 
-print(a)
-print(b)
+class Quote(BaseModel):
+    quote: str = Field(
+        description="The quote that Nicolacus Maximus said.",
+    )
+    year: int = Field(
+        description="The year when Nicolacus Maximus said the quote.",
+    )
+
+
+@app.get(
+    "/quote",
+    summary="Returns a random quote by Nicolacus Maximus",
+    description="Upon receiving a GET request this endpoint will return a real quiote said by Nicolacus Maximus himself.",
+    response_description="A Quote object that contains the quote said by Nicolacus Maximus and the date when the quote was said.",
+    response_model=Quote,
+)
+def get_quote():
+    return {
+        "quote": "Life is short so eat it all.",
+        "year": 1950,
+    }
