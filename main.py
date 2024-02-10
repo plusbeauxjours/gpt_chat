5b6f7b5160189e20106a5b08aff02bdb31ff1f4e
plusbeauxjours <plusbeauxjours@gmail.com>
2024-02-10 10:46:25 +0700

    set: API Key Auth

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel, Field
 
 app = FastAPI(
@@ -28,7 +28,8 @@ class Quote(BaseModel):
     response_description="A Quote object that contains the quote said by Leonardo da Vinci and the date when the quote was said.",
     response_model=Quote,
 )
-def get_quote():
+def get_quote(request: Request):
+    print(request.headers)
     return {
         "quote": "Life is short so eat it all.",
         "year": 1950,
